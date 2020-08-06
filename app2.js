f6c7695cd26de9485c3e9ea51cfa7f9e8efcb7ec
dhanalakshmi5k9@gmail.com <53445441+Kothadhana@users.noreply.github.com>
2020-08-06 18:56:35 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,60 @@
+function enableJob()
+{
+
+    $(".job")[0].removeAttribute("disabled");
+    $(".job")[0].value="";
+}
+
+function enableCity()
+{
+  
+   $(".city")[0].removeAttribute("disabled");
+   $(".city")[0].value="";
+}
+
+
+$('#carousel-example').on('slide.bs.carousel', function (e) {
+    /*
+        CC 2.0 License Iatek LLC 2018 - Attribution required
+    */
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 5;
+    var totalItems = $('.carousel-item').length;
+
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+
+$('#carousel-example2').on('slide.bs.carousel', function (e) {
+    /*
+        CC 2.0 License Iatek LLC 2018 - Attribution required
+    */
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 5;
+    var totalItems = $('.carousel-item2').length;
+
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item2').eq(i).appendTo('.carousel-inner2');
+            }
+            else {
+                $('.carousel-item2').eq(0).appendTo('.carousel-inner2');
+            }
+        }
+    }
+});
